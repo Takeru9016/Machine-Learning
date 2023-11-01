6947870961d61459b020d09bcf565a36fd3b6bfd
Takeru9016 <youthfox046@gmail.com>
2023-11-01 21:04:06 +0530

    Image Classification Project

---
+++ image_classify/main.py
@@ -0,0 +1,58 @@
+import os
+import pickle
+
+import numpy as np
+
+import skimage.io as io
+import skimage.transform as transform
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+# Get the current script's directory
+script_dir = os.path.dirname(os.path.abspath(
+    __file__ if '__file__' in locals() else os.path.abspath(os.path.dirname(__file__))))
+
+# Define the directory structure
+input_dir = os.path.join(script_dir, 'clf-data')
+categories = ['empty', 'not_empty']
+
+# Pepare the dataset
+data = []
+labels = []
+
+for category_idx, category in enumerate(categories):
+    category_dir = os.path.join(input_dir, category)
+    for file in os.listdir(category_dir):
+        img_path = os.path.join(category_dir, file)
+        img = io.imread(img_path)
+        img = transform.resize(img, (15, 15))
+        data.append(img.flatten())
+        labels.append(category_idx)
+
+data = np.asarray(data)
+labels = np.asarray(labels)
+
+# Train/Test split
+x_train, x_test, y_train, y_test = train_test_split(
+    data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+# Train Classifier
+classifier = SVC()
+
+parameters = [{'gamma': [0.01, 0.001, 0.0001], 'C': [1, 10, 100, 1000]}]
+
+grid_search = GridSearchCV(classifier, parameters, cv=5)
+grid_search.fit(x_train, y_train)
+
+# Test Performance
+best_estimator = grid_search.best_estimator_
+
+y_prediction = best_estimator.predict(x_test)
+
+score = accuracy_score(y_prediction, y_test)
+
+print('{}% of samples were correctly classified'.format(str(score * 100)))
+
+pickle.dump(best_estimator, open('./model.p', 'wb'))
